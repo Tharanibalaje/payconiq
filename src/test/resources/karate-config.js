a34ad9cea88fb75de01a03903fc2302274cc448a
Tharanibalaje <53902380+Tharanibalaje@users.noreply.github.com>
2019-08-14 17:40:41 +0200

    removed proxy settings

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,7 +1,6 @@
 function fn() {
   karate.configure('connectTimeout', 5000);
   karate.configure('readTimeout', 5000);
-  karate.configure('proxy', 'http://nl-userproxy-access.net.abnamro.com:8080');
   var config = { demoBaseUrl: 'https://api.github.com' };
     config.token = '39b81eea3d5be1f08eb688344127bc0b165d1183';
     config.authorization = 'Basic dGhhcmFuaWJhbGFqZTozOWI4MWVlYTNkNWJlMWYwOGViNjg4MzQ0MTI3YmMwYjE2NWQxMTgz'
